679eb0ed765caaded2037aa6ff32d52ac07e58f7
PieterQQ <pieter444@wp.pl>
2020-07-05 20:54:18 +0200

    TodoServicesto fix

--- TODOList/TODOList.Application/Interfaces/ITodoServices.cs
+++ TODOList/TODOList.Application/Interfaces/ITodoServices.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TODOList.Application.ViewModels;
 
 namespace TODOList.Application.Interfaces
 {
     public interface ITodoServices
     {
+        Task<TodoListListVm> GetAllTodoLists();
+        Task<TodoListVm> GetTodoListsById(int id);
+        Task<TodoItemListVm> GetTodoItemsForList(int listId);
+        Task<TodoItemListVm> GetAllTodoItems(int id);
+        Task<int> InsertTodoList(TodoListVm todoList);
+        Task DeleteTodoList(int todolistid);
+     
+        Task<int> InsertTodoItem(TodoItemVm todoItem);
+        Task DeleteTodoItem(int todoItemid);
+        Task UpdateTodoItem(TodoItemVm todoItem);
+    
+
     }
 }
--- TODOList/TODOList.Application/Services/TodoServices.cs
+++ TODOList/TODOList.Application/Services/TodoServices.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using TODOList.Application.Interfaces;
 using TODOList.Application.ViewModels;
 using TODOList.Domain.Interfaces;
@@ -12,15 +16,77 @@ namespace TODOList.Application.Services
     {
         private readonly ITodoItemRepository _itemrepo;
         private readonly ITodoListRepository _listRepo;
-        public TodoServices(ITodoItemRepository itemrepo,ITodoListRepository listRepo)
+        private readonly IMapper _mapper;
+        public TodoServices(ITodoItemRepository itemrepo, ITodoListRepository listRepo, IMapper mapper)
         {
             _itemrepo = itemrepo;
             _listRepo = listRepo;
+            _mapper = mapper;
         }
-        //public List<TodoListVm> GetListWithItems()
-        //{
-        //    var lists = _listRepo.GetAll();
-        //    var items = _itemrepo.GetAll();
-        //}
+
+        public async Task DeleteTodoItem(int todoItemid)
+        {
+            await _itemrepo.DeleteTodoItem(todoItemid);
+        }
+
+        public async Task DeleteTodoList(int todolistid)
+        {
+            await _listRepo.DeleteTodoList(todolistid);
+        }
+
+        public async Task<TodoItemListVm> GetAllTodoItems(int id)
+        {
+            var items = await _itemrepo.GetAll().ProjectTo<TodoItemVm>(_mapper.ConfigurationProvider).ToListAsync();
+            return new TodoItemListVm()
+            {
+                Items = items,
+                Count = items.Count
+            };
+        }
+
+        public async Task<TodoListListVm> GetAllTodoLists()
+        {
+          var lists=await _listRepo.GetAll().ProjectTo<TodoListVm>(_mapper.ConfigurationProvider).ToListAsync();
+            return new TodoListListVm()
+            {
+              Lists=lists,
+              Count=lists.Count
+            };
+        }
+
+        public async Task<TodoItemListVm> GetTodoItemsForList(int listId)
+        {
+            var items = await _itemrepo.GetTodoItemsForList(listId).ProjectTo<TodoItemVm>(_mapper.ConfigurationProvider).ToListAsync();
+            return new TodoItemListVm()
+            {
+                Items = items,
+                Count = items.Count
+            };
+        }
+
+        public async Task<TodoListVm> GetTodoListsById(int id)
+        {
+            var list = await _listRepo.GetListById(id).ProjectTo<TodoListVm>(_mapper.ConfigurationProvider).SingleOrDefaultAsync();
+            return new TodoListListVm()
+            {
+                Lists = lists,
+                Count = lists.Count
+            };
+        }
+
+        public Task<int> InsertTodoItem(TodoItemVm todoItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertTodoList(TodoListVm todoList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTodoItem(TodoItemVm todoItem)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
